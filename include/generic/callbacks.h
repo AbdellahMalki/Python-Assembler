102712721c41d56faa8134c85a6db6e65d2628c8
Abdellah Malki <166424842+AbdellahMalki@users.noreply.github.com>
2026-02-05 00:08:27 +0100

    Create callbacks.h

---
+++ include/generic/callbacks.h
@@ -0,0 +1,23 @@
+
+/**
+ * @file callbacks.h
+ * @author François Cayre <francois.cayre@grenoble-inp.fr>
+ * @brief Callbacks.
+ *
+ * Callbacks.
+ */
+
+#ifndef CALLBACKS_H
+#define CALLBACKS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  typedef int (*action_t)( void* );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CALLBACKS_H_ */
